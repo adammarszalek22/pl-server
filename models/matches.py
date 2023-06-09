720350ab8cf65accd3218664bb19c9d222535901
Adam Marszalek <adammarszalek60@gmail.com>
2023-06-09 19:46:04 +0100

    trying smth

--- models/matches.py
+++ models/matches.py
@@ -4,6 +4,7 @@ from db import db
 class MatchesModel(db.Model):
     __tablename__ = 'matches'
 
+    id = db.Column(db.Integer, primary_key=True)
     match_id = db.Column(db.String(80))
     goal1 = db.Column(db.Integer)
     goal2 = db.Column(db.Integer)
