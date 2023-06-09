c6615cf18cf8b198edf8658e8e2a6601e1ab178a
Adam Marszalek <adammarszalek60@gmail.com>
2023-06-09 18:39:41 +0100

    trying smth

--- settings.py
+++ settings.py
@@ -1,8 +1,4 @@
 import os
-import redis
-from rq_scheduler import Scheduler
-from datetime import datetime
-from tasks import example
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,14 +6,3 @@ load_dotenv()
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
 QUEUES = ["example", "default"]
 
-connection = redis.from_url(
-            os.getenv("REDIS_URL")
-)
-scheduler = Scheduler('example', connection=connection)
-#scheduler.enqueue_in(timedelta(seconds=10), example)
-scheduler.schedule(
-    scheduled_time=datetime.utcnow(),
-    func=example,
-    interval=10,
-    repeat=10
-    )
--- tasks.py
+++ tasks.py
@@ -7,7 +7,23 @@ from passlib.hash import pbkdf2_sha256
 from models import UserModel
 from db import db
 
+import redis
+from datetime import datetime
+from rq_scheduler import Scheduler
+
 load_dotenv()
 
 def example():
     print("Just seeing how this works.", file=sys.stderr)
+
+connection = redis.from_url(
+            os.getenv("REDIS_URL")
+)
+scheduler = Scheduler('example', connection=connection)
+#scheduler.enqueue_in(timedelta(seconds=10), example)
+scheduler.schedule(
+    scheduled_time=datetime.utcnow(),
+    func=example,
+    interval=10,
+    repeat=10
+    )
